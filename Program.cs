e7574d734fd0a29089855526f063e796121ec80e
rastogi102 <nikhilrastogi102@gmail.com>
2023-07-18 14:11:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppEnum
+{
+    internal class Program
+    {
+        public enum DayofWeek
+        {
+            Monday=1, Tuesday=2, Wednesday=3,Thursday,Friday,Saturday
+        }
+        static void Main(string[] args)
+        {
+            DayofWeek today = DayofWeek.Tuesday;
+            Console.WriteLine("Day:"+today);
+            Console.WriteLine("Day Number"+(int)today);
+            Console.ReadKey();
+        }
+    }
+}
